212ba1483054ddddc4d7d54cff9b72e1c8312f26
Shrena Reddy.M <24wh1a05d4@bvrithyderabad.edu.in>
2026-07-04 09:58:51 +0530

    Added API to generate random users

--- frontend/index.js
+++ frontend/index.js
@@ -12,6 +12,7 @@ const users = [
 ]
 let curUserId = 0;
 function toggle(){
+
   if(curUserId == 0)
     curUserId=1;
   else
@@ -25,3 +26,23 @@ document.getElementById("card-gender").innerText = users[curUserId].gender;
 }
 
 console.log("Hello from JS");
+
+function getRandomUser()
+{
+  fetch("https://randomuser.me/api")
+    .then(function (data){
+      return data.json()
+    })
+    .then(function (parsedData){
+      let gender = parsedData.results[0].gender;
+      let first = parsedData.results[0].name.first;
+      let last = parsedData.results[0].name.last;
+      let name = first  + " " + last;
+      let imageUrl = parsedData.results[0].picture.large;
+
+      document.getElementById("card-gender").innerText=gender;
+      document.getElementById("card-name").innerText=name
+      document.getElementById("img").src=imageUrl
+
+    })
+}
